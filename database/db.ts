d561425a3d932275a501925dce2938cdfe8701fc
hunghoang <hth321@gmail.com>
2025-12-12 14:36:46 +0700

    refactor: Disable change listener for database connection in db.ts

--- database/db.ts
+++ database/db.ts
@@ -3,5 +3,7 @@ import { openDatabaseSync } from 'expo-sqlite';
 
 export const DATABASE_NAME = 'babyease.db';
 
-export const expoDb = openDatabaseSync(DATABASE_NAME, { enableChangeListener: true });
+export const expoDb = openDatabaseSync(DATABASE_NAME, {
+  enableChangeListener: false, // https://expo.dev/blog/modern-sqlite-for-react-native-apps we don't need change listener
+});
 export const db = drizzle(expoDb);
